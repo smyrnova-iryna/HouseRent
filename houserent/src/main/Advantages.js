0350d0b0f9ab63e2eb0950eec4f40d67b680f7a9
Ira1234567 <iraowl1234@gmail.com>
2023-11-23 10:48:01 +0200

    Update Advantages component

--- houserent/src/main/Advantages.js
+++ houserent/src/main/Advantages.js
@@ -61,8 +61,8 @@ const Advantages = ({data}) => {
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit odit iure nisi cum dolore beatae, laudantium veritatis fugiat, saepe accusantium odio qui debitis est, facere ea exercitationem delectus soluta minima a cumque? Laborum reprehenderit, perspiciatis fugit repellendus et necessitatibus tempore inventore blanditiis libero odio odit velit doloremque nesciunt commodi nobis quod voluptatum ex ipsa? Magni, nemo! Optio repellendus ea dolorum, omnis iusto ex, excepturi eaque magni iure quae harum veniam eum, vitae quo natus atque non culpa aut libero dicta necessitatibus magnam ullam commodi. Neque, maiores reiciendis? Cumque, suscipit magnam vero eveniet vel earum nobis doloribus dignissimos culpa, natus quia reiciendis neque optio et qui accusantium provident amet fugiat dolore recusandae! Possimus, cupiditate. Veniam, enim id tenetur dicta excepturi beatae dolorum reprehenderit in veritatis consectetur sunt! Sint, illo tempora dolor laboriosam odit tenetur reiciendis velit omnis accusamus? Atque officiis tenetur dolore corrupti voluptates mollitia blanditiis rerum libero odio, vero, perspiciatis similique maiores commodi cupiditate optio, soluta beatae accusamus minima explicabo. Sit quis enim nostrum, ad perspiciatis qui? Consectetur neque quasi illum enim rem nemo pariatur labore laudantium velit nam nostrum impedit libero, quae, aperiam unde, et harum dolore! Facere enim a eos pariatur officiis magnam quam veniam sint necessitatibus voluptatum?</p>
             </div> */}
             <div className="Main-carousel-container">
-            <div className="mainPageContainer">
-                   <div>
+            {/* <div> */}
+                   {/* <div className="Main-carousel-inner-container"> */}
                         <img className='Slider-image Slider-image9' src={require("../data/carousel/carousel_photo_1.jpg")} alt="Carousel item 1" />
                         <img className='Slider-image Slider-image8' src={require("../data/carousel/carousel_photo_2.jpg")} alt="Carousel item 2" />
                         <img className='Slider-image Slider-image7' src={require("../data/carousel/carousel_photo_3.jpg")} alt="Carousel item 3" />
@@ -72,8 +72,8 @@ const Advantages = ({data}) => {
                         <img className='Slider-image Slider-image3' src={require("../data/carousel/carousel_photo_7.jpg")} alt="Carousel item 7" />
                         <img className='Slider-image Slider-image2' src={require("../data/carousel/carousel_photo_8.jpg")} alt="Carousel item 8" />
                         <img className='Slider-image Slider-image1' src={require("../data/carousel/carousel_photo_9.jpg")} alt="Carousel item 9" />
-                    </div>
-            </div>
+                    {/* </div> */}
+            {/* </div> */}
             </div>
             <div>
                 <p className="Advantages-list-title">{data.advantages.mainTitle}</p>
