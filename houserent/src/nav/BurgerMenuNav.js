cafd7eb9609dd80d486a7ce05da476d2544ac2f5
Ira1234567 <iraowl1234@gmail.com>
2024-02-26 12:22:20 +0200

    Update hashlinks

--- houserent/src/nav/BurgerMenuNav.js
+++ houserent/src/nav/BurgerMenuNav.js
@@ -7,9 +7,9 @@ const BurgerMenuNav = ({data, navClassName, hideBurgerMenuNav, navigateToTheTop}
     return (
         <nav className={navClassName}>
             <Link className='Nav-inner-link' to={data.menuPaths.main} onClick={() => {hideBurgerMenuNav(); navigateToTheTop()}}>{data.menuItems.main}</Link>
-            <HashLink smooth to={data.menuPaths.advantages} className='Nav-inner-link' onClick={() => {hideBurgerMenuNav(); navigateToTheTop()}}>{data.menuItems.advantages}</HashLink>
-            <HashLink smooth to={data.menuPaths.offices} className='Nav-inner-link' onClick={() => {hideBurgerMenuNav(); navigateToTheTop()}}>{data.menuItems.offices}</HashLink>
-            <HashLink smooth to={data.menuPaths.contacts} className='Nav-inner-link' onClick={() => {hideBurgerMenuNav(); navigateToTheTop()}}>{data.menuItems.contacts}</HashLink>
+            <HashLink smooth to={data.menuPaths.advantages} className='Nav-inner-link' onClick={() => {hideBurgerMenuNav()}}>{data.menuItems.advantages}</HashLink>
+            <HashLink smooth to={data.menuPaths.offices} className='Nav-inner-link' onClick={() => {hideBurgerMenuNav()}}>{data.menuItems.offices}</HashLink>
+            <HashLink smooth to={data.menuPaths.contacts} className='Nav-inner-link' onClick={() => {hideBurgerMenuNav()}}>{data.menuItems.contacts}</HashLink>
         </nav>
     );
     }
--- houserent/src/offices/SecondFloor.js
+++ houserent/src/offices/SecondFloor.js
@@ -40,7 +40,7 @@ const SecondFloor = ({data, navigateToTheTop}) => {
     // }
 
     return (
-        <div id="third_floor" className="Floor-container" ref={comp}>
+        <div id="second_floor" className="Floor-container" ref={comp}>
             <div className="Floor-title-container">
                 <div className="Floor-title-icon-container">
                     <img className="House-icon" src={require("../data/icons/house_icon.png")} alt="House icon"></img>
